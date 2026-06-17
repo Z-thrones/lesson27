84d9be89bc970d782e5122908e6adfc1d1e5b6ff
Z-thrones <zaviyar.ktk@gmail.com>
2026-06-17 12:51:20 +0500

    Add files via upload

---
+++ A69.py
@@ -0,0 +1,10 @@
+empty_list = []
+print()
+numbers = [1 , 2 , 3 , 4 , 5]
+print(numbers)
+triples = [1 , 2 , 3]
+triples2 = triples * 3
+print(triples2)
+alist = [100 , 200 , 300 , 400 , 500]
+alistr = alist[::-1]
+print(alistr)
